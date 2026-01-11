1def6f8f88da442b7bf7820c2e5e20aeb419d773
NTWKKM <NTWKKM@users.noreply.github.com>
2026-01-11 14:24:50 +0700

    fix action

--- tests/unit/test_advanced_stats.py
+++ tests/unit/test_advanced_stats.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add project root to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 import pytest
 import pandas as pd
 import numpy as np
